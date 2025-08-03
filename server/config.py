5d1cc759bfdbf8a176b4c2fd5b448a62f9021dc4
Rohit Choukiker <rohitchoukiker2803@gmail.com>
2025-08-03 09:20:44 +0530

    new version is comming

--- server/config.py
+++
@@ -1,3 +0,0 @@
-VECTOR_STORE_PATH = "./faiss_index"
-EMBEDDING_MODEL = "all-MiniLM-L6-v2"
-HF_LLM = "google/flan-t5-small"
--- server/utils/helper.py
+++
@@ -1,6 +0,0 @@
-def extract_metadata(df, sheet_name, row_index):
-    return {
-        "sheet": sheet_name,
-        "row": row_index,
-        "columns": list(df.columns)
-    }
